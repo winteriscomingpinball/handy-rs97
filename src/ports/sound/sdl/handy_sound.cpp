974af0abff77156c20024a9b69527e834eee3a8c
winteriscomingpinball <silentcatalyst@gmail.com>
2021-03-11 15:33:10 -0500

    Update handy_sound.cpp

--- src/ports/sound/sdl/handy_sound.cpp
+++ src/ports/sound/sdl/handy_sound.cpp
@@ -41,45 +41,16 @@ static uint32_t buf_read_pos = 0;
 static uint32_t buf_write_pos = 0;
 static int32_t buffered_bytes = 0;
 
+static volatile int audio_done;
 
-static int32_t sdl_read_buffer(uint8_t* data, int32_t len)
-{
-	if (buffered_bytes >= len) 
-	{
-		if(buf_read_pos + len <= BUFFSIZE ) 
-		{
-			memcpy(data, gAudioBuffer + buf_read_pos, len);
-		} 
-		else 
-		{
-			int32_t tail = BUFFSIZE - buf_read_pos;
-			memcpy(data, gAudioBuffer + buf_read_pos, tail);
-			memcpy(data + tail, gAudioBuffer, len - tail);
-		}
-		buf_read_pos = (buf_read_pos + len) % BUFFSIZE;
-		buffered_bytes -= len;
-	}
-
-	return len;
-}
-
-static void sdl_write_buffer(uint8_t* data, int32_t len)
-{
-	for(uint32_t i = 0; i < len; i += 4) 
-	{
-		if(buffered_bytes == BUFFSIZE) return; // just drop samples
-		*(int32_t*)((char*)(buffer + buf_write_pos)) = *(int32_t*)((char*)(data + i));
-		//memcpy(buffer + buf_write_pos, data + i, 4);
-		buf_write_pos = (buf_write_pos + 4) % BUFFSIZE;
-		buffered_bytes += 4;
-	}
-}
 
 
 
 void sdl_callback(void *unused, uint8_t *stream, int32_t len)
 {
-	sdl_read_buffer((uint8_t *)stream, len);
+	//sdl_read_buffer((uint8_t *)stream, len);
+	memcpy(stream, gAudioBuffer, len);
+	audio_done = 1;
 }
 
 
@@ -161,9 +132,14 @@ void handy_audio_loop()
 		uint32_t f = gAudioBufferPointer;
 		gAudioBufferPointer = 0;	
 		long ret, len = f / 4;
-		SDL_LockAudio();
-		sdl_write_buffer(gAudioBuffer, len);
-		SDL_UnlockAudio();
+		
+		while (!audio_done)
+			SDL_Delay(4);
+		audio_done = 0;
+	
+		//SDL_LockAudio();
+		//sdl_write_buffer(gAudioBuffer, len);
+		//SDL_UnlockAudio();
 		gAudioBufferPointer = 0;	
 		
 	}
