778460da762c321e7ecd4b8e4672a83b84bb0604
winteriscomingpinball <silentcatalyst@gmail.com>
2021-03-11 15:21:09 -0500

    Update handy_sound.cpp

--- src/ports/sound/sdl/handy_sound.cpp
+++ src/ports/sound/sdl/handy_sound.cpp
@@ -159,9 +159,9 @@ void handy_audio_loop()
 	if (gAudioBufferPointer >= HANDY_AUDIO_BUFFER_SIZE/2 && gAudioEnabled)
 	{
 		uint32_t f = gAudioBufferPointer;
-		SDL_LockAudio();
-		sdl_write_buffer(gAudioBuffer, HANDY_AUDIO_BUFFER_SIZE * 4);
-		SDL_UnlockAudio();
+		//SDL_LockAudio();
+		//sdl_write_buffer(gAudioBuffer, HANDY_AUDIO_BUFFER_SIZE * 4);
+		//SDL_UnlockAudio();
 		gAudioBufferPointer = 0;	
 		
 	}
