3adc6c81ff5a278a83aa8af523d3569fb3d3834e
winteriscomingpinball <silentcatalyst@gmail.com>
2021-03-11 13:15:54 -0500

    Update handy_sound.cpp

--- src/ports/sound/sdl/handy_sound.cpp
+++ src/ports/sound/sdl/handy_sound.cpp
@@ -32,35 +32,50 @@
 #include "handy_sdl_main.h"
 #include "handy_sound.h"
 
+//static snd_pcm_t *handle;
 
+
+static int32_t BUFFSIZE;
+static uint8_t *buffer;
 static uint32_t buf_read_pos = 0;
 static uint32_t buf_write_pos = 0;
 static int32_t buffered_bytes = 0;
-
-//static snd_pcm_t *handle;
-
 
 
 static int32_t sdl_read_buffer(uint8_t* data, int32_t len)
 {
 	if (buffered_bytes >= len) 
 	{
-		if(buf_read_pos + len <= HANDY_AUDIO_BUFFER_SIZE ) 
+		if(buf_read_pos + len <= BUFFSIZE ) 
 		{
-			memcpy(data, gAudioBuffer + buf_read_pos, len);
+			memcpy(data, buffer + buf_read_pos, len);
 		} 
 		else 
 		{
-			int32_t tail = HANDY_AUDIO_BUFFER_SIZE - buf_read_pos;
-			memcpy(data, gAudioBuffer + buf_read_pos, tail);
-			memcpy(data + tail, gAudioBuffer, len - tail);
+			int32_t tail = BUFFSIZE - buf_read_pos;
+			memcpy(data, buffer + buf_read_pos, tail);
+			memcpy(data + tail, buffer, len - tail);
 		}
-		buf_read_pos = (buf_read_pos + len) % HANDY_AUDIO_BUFFER_SIZE;
+		buf_read_pos = (buf_read_pos + len) % BUFFSIZE;
 		buffered_bytes -= len;
 	}
 
 	return len;
 }
+
+static void sdl_write_buffer(uint8_t* data, int32_t len)
+{
+	for(uint32_t i = 0; i < len; i += 4) 
+	{
+		if(buffered_bytes == BUFFSIZE) return; // just drop samples
+		*(int32_t*)((char*)(buffer + buf_write_pos)) = *(int32_t*)((char*)(data + i));
+		//memcpy(buffer + buf_write_pos, data + i, 4);
+		buf_write_pos = (buf_write_pos + 4) % BUFFSIZE;
+		buffered_bytes += 4;
+	}
+}
+
+
 
 void sdl_callback(void *unused, uint8_t *stream, int32_t len)
 {
@@ -69,60 +84,60 @@ void sdl_callback(void *unused, uint8_t *stream, int32_t len)
 
 
 
-static void sdl_write_buffer(uint8_t* data, int32_t len)
-{
-	for(uint32_t i = 0; i < len; i += 4) 
-	{
-		if(buffered_bytes == HANDY_AUDIO_BUFFER_SIZE) return; // just drop samples
-		*(int32_t*)((char*)(gAudioBuffer + buf_write_pos)) = *(int32_t*)((char*)(data + i));
-		//memcpy(buffer + buf_write_pos, data + i, 4);
-		buf_write_pos = (buf_write_pos + 4) % HANDY_AUDIO_BUFFER_SIZE;
-		buffered_bytes += 4;
-	}
-}
-
-
-
-
 int handy_audio_init(void)
 {
     /* If we don't want sound, return 0 */
     if(gAudioEnabled == FALSE) return 0;
 
-    SDL_AudioSpec aspec, obtained;
+#ifdef HANDY_SDL_DEBUG
+    printf("handy_audio_init - DEBUG\n");
+#endif
 
-	/* Add some silence to the buffer */
-	//buffered_bytes = 0;
-	//buf_read_pos = 0;
-	//buf_write_pos = 0;
+    //BUFFSIZE = (HANDY_AUDIO_BUFFER_SIZE * 2 * 2) * 4;
+	BUFFSIZE = HANDY_AUDIO_BUFFER_SIZE * 4;
+	
+	buffer = (uint8_t *) malloc(BUFFSIZE);
 
+    /* Add some silence to the buffer */
+	buffered_bytes = 0;
+	buf_read_pos = 0;
+	buf_write_pos = 0;
+    
+
+	//snd_pcm_hw_params_t *params;
+	//uint32_t val;
+	//int32_t dir = -1;
+	//snd_pcm_uframes_t frames;
+	
+	SDL_AudioSpec aspec, obtained;
+	
+	
 	aspec.format   = AUDIO_S16SYS;
 	aspec.freq     = HANDY_AUDIO_SAMPLE_FREQ;
 	aspec.channels = 2;
 	aspec.samples  = HANDY_AUDIO_BUFFER_SIZE;
 	aspec.callback = (sdl_callback);
 	aspec.userdata = NULL;
-
+	
+	
+	
 	/* initialize the SDL Audio system */
 	if (SDL_InitSubSystem (SDL_INIT_AUDIO | SDL_INIT_NOPARACHUTE)) 
 	{
 		printf("SDL: Initializing of SDL Audio failed: %s.\n", SDL_GetError());
 		return 0;
 	}
-
-	/* Open the audio device and start playing sound! */
+	
+	
 	if(SDL_OpenAudio(&aspec, &obtained) < 0) 
 	{
 		printf("SDL: Unable to open audio: %s\n", SDL_GetError());
 		return 0;
 	}
 	
+	gAudioEnabled = 1;
 	SDL_PauseAudio(0);
 	
-	
-	
-	
-	gAudioEnabled = 1;
     return 1;
 }
 
@@ -131,22 +146,23 @@ void handy_audio_close()
 	SDL_PauseAudio(1);
 	SDL_CloseAudio();
 	SDL_QuitSubSystem(SDL_INIT_AUDIO);
-	//if (buffer)
-	//{
-	//	free(buffer);	
-	//	buffer = NULL;
-	//}
+	if (buffer)
+	{
+		free(buffer);	
+		buffer = NULL;
+	}
 }
 
 void handy_audio_loop()
 {
 	mpLynx->Update();
-	//if (gAudioBufferPointer >= HANDY_AUDIO_BUFFER_SIZE/2 && gAudioEnabled)
-	//{
-	SDL_LockAudio();
-	sdl_write_buffer(gAudioBuffer, HANDY_AUDIO_BUFFER_SIZE * 4);
-	SDL_UnlockAudio();
-	//}
-	
+	if (gAudioEnabled)
+	{
+		uint32_t f = gAudioBufferPointer;
+		SDL_LockAudio();
+		sdl_write_buffer(gAudioBuffer, HANDY_AUDIO_BUFFER_SIZE * 4);
+		SDL_UnlockAudio();
+		gAudioBufferPointer = 0;	
+		
+	}
 }
-
