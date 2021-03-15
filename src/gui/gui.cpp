b4042a967d591ff95484afbacc8ea31a3acad035
winteriscomingpinball <silentcatalyst@gmail.com>
2021-03-15 16:07:43 -0400

    Update gui.cpp

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -318,9 +318,9 @@ void ShowChar(SDL_Surface *s, int x, int y, unsigned char a, int fg_color, int b
 	}else{
 		
 		
-			for(h = 9*size; h; h--) {
+			for(h = 8*size; h; h--) {
 				dst = (Uint16 *)s->pixels + (y+(8*size)-h)*s->w + x;
-				charPixelh=h/size+1;
+				charPixelh=h/size;
 				for(w = 8*size; w; w--) {
 					charPixelw=w/size+1;
 					Uint16 color = bg_color; // background
