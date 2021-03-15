77936036eb5b790a1840bda91df45ffe62cf3b3e
winteriscomingpinball <silentcatalyst@gmail.com>
2021-03-15 15:29:59 -0400

    Update gui.cpp

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -406,7 +406,7 @@ void gui_SaveState()
 void print_string(const char *s, u16 fg_color, u16 bg_color, int x, int y, int size)
 {
 	int i, j = strlen(s);
-	for(i = 0; i < j; i++, x += 8*size) ShowChar(menuSurface, x, y, s[i], fg_color, bg_color, size);
+	for(i = 0; i < j; i++, x += 7*size) ShowChar(menuSurface, x, y, s[i], fg_color, bg_color, size);
 }
 
 /*
