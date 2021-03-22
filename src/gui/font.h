6d34ac4476223e285b17b220d72a40b1cadd0f0c
winteriscomingpinball <silentcatalyst@gmail.com>
2021-03-22 08:40:16 -0400

    Update font.h

--- src/gui/font.h
+++ src/gui/font.h
@@ -6,5 +6,5 @@ extern unsigned char console_font_6x8[2048];
 
 #define USED_FONT console_font_6x8
 #define USED_FONT_SIZE 2
-#define USED_FONT_WIDTH 7
+#define USED_FONT_WIDTH 8
 #define MAX_CHARACTERS_ACROSS 20
