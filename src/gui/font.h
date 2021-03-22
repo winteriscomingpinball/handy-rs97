821f70951d66fb2b46507b9e338740d96e4f4a2a
winteriscomingpinball <silentcatalyst@gmail.com>
2021-03-22 08:30:35 -0400

    Update font.h

--- src/gui/font.h
+++ src/gui/font.h
@@ -1,10 +1,10 @@
 #define FONT_WIDTH 5
 
 //extern unsigned char gui_font[2048];
-//extern unsigned char console_font_6x8[2048];
-extern unsigned char console_font_5x8[2048];
+extern unsigned char console_font_6x8[2048];
+//extern unsigned char console_font_5x8[2048];
 
-#define USED_FONT console_font_5x8
+#define USED_FONT console_font_6x8
 #define USED_FONT_SIZE 2
-#define USED_FONT_WIDTH 6
-#define MAX_CHARACTERS_ACROSS 24
+#define USED_FONT_WIDTH 7
+#define MAX_CHARACTERS_ACROSS 20
