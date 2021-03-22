2d41210be1f3dc8f8836dec3f50224dc817cb8ab
winteriscomingpinball <silentcatalyst@gmail.com>
2021-03-22 08:49:40 -0400

    Update gui.cpp

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -441,9 +441,9 @@ void print_string(const char *s, u16 fg_color, u16 bg_color, int x, int y, int s
 	int sizemod=0;
 	int i, j = strlen(s);
 	if (size>1){
-		sizemod=FONT_WIDTH*size; //font width X size
+		sizemod=USED_FONT_WIDTH*size; //font width X size
 	}else{
-	    sizemod=FONT_WIDTH+1; //font width + 1
+	    sizemod=USED_FONT_WIDTH+1; //font width + 1
 	}
 	 
 
